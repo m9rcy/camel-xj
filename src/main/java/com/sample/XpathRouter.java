eb054347c38f492ca9e3eea9954f2492c69b4db4
m9rcy <m9rcy19@gmail.com>
2025-06-04 10:53:03 +1200

    Updates

--- src/main/java/com/sample/XpathRouter.java
+++ src/main/java/com/sample/XpathRouter.java
@@ -10,14 +10,14 @@ public class XpathRouter extends RouteBuilder {
     Namespaces namespaces = new Namespaces("ns", "http://example.com");
     @Override
     public void configure() throws Exception {
-        from("file:src/main/resources?fileName=sample-input3.xml&noop=true")
+        from("file:src/main/resources?fileName=sample-input4.xml&noop=true")
                 .routeId("test-file-to-direct")
                 .log("Loaded test XML file")
                 .to("direct:start");
 
         from("direct:start")
                 .choice()
-                .when(xpath("/ns:QueryResponse[@rsCount > 0 or @rsTotal > 0]", namespaces))
+                .when(xpath("/ns:QueryResponse[@rsCount > 0 or @rsTotal > 0] and ns:TP_OB_SET/ns:TPBLOCK[ns:STATUS='COMMISSION']", namespaces))
                 .log("Response is valid and has records")
                 .setHeader("parentId", constant("AEST_1_NEW"))
                 .to("xslt:classpath:transform-commission.xsl")
